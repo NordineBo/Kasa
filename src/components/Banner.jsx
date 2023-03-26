469a2667b6f9bd65cedaae8ba9016455e7c41dec
Nordine Boukahilat <nordineboukahilat@gmail.com>
2023-03-26 17:58:27 +0200

    first routeur

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+        <img src="Banner.png" alt="banner" width="1000" />
+    </div>
+  )
+}
+
+export default Banner
